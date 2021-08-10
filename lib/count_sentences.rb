db25198cac6b949353a0ed03c3b0f3c6a9d73846
nhuse <nathan.huse@yahoo.com>
2021-08-10 13:41:09 -0500

    commit tree 1540e2a721d0df72c35525aae49091d9f851a7d1

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_arr = self.split(/[.!?]/)
+    new_split_arr = split_arr.filter do |sentence|
+      !(sentence.empty?)
+    end
+    new_split_arr.length
   end
 end
